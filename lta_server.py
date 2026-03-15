2c962119483768db4d2d72b85ab92c99ace77be5
Minhhhh <mineskiroxor@gmail.com>
2026-03-15 15:24:06 +0800

    chore: format files

--- lta_server.py
+++ lta_server.py
@@ -1,28 +1,23 @@
 from mcp.server.fastmcp import FastMCP
-from lib.lta import LTAService
-import logging
 
-mcp = FastMCP(
-    name='lta',
-    stateless_http=True,
-    host="127.0.0.1", port=8000
-)
+from lib.lta import LTAService
+
+mcp = FastMCP(name="lta", stateless_http=True)
 
 lta_service = LTAService()
+
 
 @mcp.tool()
 async def get_bus_stops() -> str:
     """Get detailed information for all bus stops that includes:
-        1. Bus stop code
-        2. Road name
-        3. Description to identify the bus stop
-        4. Latitude and Longitude
+    1. Bus stop code
+    2. Road name
+    3. Description to identify the bus stop
+    4. Latitude and Longitude
     """
     result = await lta_service.list_bus_stops()
-    logging.info(f'API Result: {result}')
     return result
 
+
 if __name__ == "__main__":
-    mcp.run(
-        transport="streamable-http"
-    )
+    mcp.run(transport="streamable-http")
